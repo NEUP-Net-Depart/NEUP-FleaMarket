6e3842cc67ab083c742c88aca8b108a3d9838417
onionwyl <onionwyl@qq.com>
2016-06-12 23:14:41 +0800

    Fix a Capital letter error

--- database/migrations/2016_03_28_222053_create_user_info_table.php
+++ database/migrations/2016_03_28_222053_create_user_info_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUserinfoTable extends Migration
+class CreateUserInfoTable extends Migration
 {
     /**
      * Run the migrations.
