04aac3d21548effdccfef418721d42e31fe36b78
koooyf <kyf19970408@163.com>
2016-08-05 01:09:29 +0800

    Add userinformation page Add edit userinformation page Add Middleware Let password encrypted

---
+++ resources/views/User/editinfo.blade.php
@@ -0,0 +1,33 @@
+{!! csrf_field() !!}
+<html>
+<head>
+    <title>修改个人信息</title>
+</head>
+<body>
+<form action="/user/{{$user->id}}/edit/middle" method="POST">
+    <div>
+        性别:<select name="gender">
+            <option value="男">男</option>
+            <option value="女">女</option>
+        </select>
+    </div>
+    </br>
+    <div>
+        真实姓名：<input type="text" name="realname">
+    </div>
+    </br>
+    <div>
+        手机号：<input type="text" name='tel_num'>
+    </div>
+    </br>
+    <div>
+        地址:<input type="text" name="address">
+    </div>
+    </br>
+    {!! csrf_field() !!}
+    <div>
+        <input type="submit" name="submit" value="修改">
+    </div>
+</form>
+</body>
+</html>
---
+++ resources/views/User/userinfo.blade.php
@@ -0,0 +1,14 @@
+性别：{{$user->gender}}
+</br>
+</br>
+真实姓名：{{$user->realname}}
+</br>
+</br>
+手机号：{{$user->tel_num}}
+</br>
+</br>
+地址：{{$user->address}}
+</br>
+</br>
+<a href='/user/{{$user->id}}/edit'>Edit</a><br/>
+
