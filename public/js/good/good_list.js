74573c61ea9f857ba06cefb232c0004a76d48bd9
iborvs <dlqqppw@163.com>
2017-05-02 17:34:43 +0800

    why it keeps twinkle..

--- public/js/good/good_list.js
+++ public/js/good/good_list.js
@@ -1,15 +1,15 @@
 $(".good").mouseenter(function(){
-    $(this).animate({opacity:'0.5'},10.0);
+    $(this).animate({opacity:'0.5'},"fast");
 });
 $(".good").mouseleave(function(){
-    $(".good").animate({opacity:'1.0'},10.0);
+    $(".good").animate({opacity:'1.0'},"fast");
 });
 $(".cat").mouseenter(function(){
-    $(this).animate({backgroundColor:'#3399CC'},10.0);
-    $("a",this).animate({color:"#FFFFFF"},10.0);
+    $(this).animate({backgroundColor:'#3399CC'},"fast");
+    $("a",this).animate({color:"#FFFFFF"},"fast");
 
 });
 $(".cat").mouseleave(function(){
-    $(this).animate({backgroundColor:'#FFFFFF'},10.0);
-    $("a",this).animate({color:"#1779ba"},10.0);
+    $(this).animate({backgroundColor:'#FFFFFF'},"fast");
+    $("a",this).animate({color:"#1779ba"},"fast");
 });
