d47acbdb41548519846b40ef992a2de30ace0459
catsworld <519418441@qq.com>
2017-05-20 14:51:15 +0000

    It seemed like that I fixed the bugs of the header

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -6,29 +6,22 @@
     </div>
     <div class="row">
       <div class="medium-10 medium-offset-1 columns">
-        <div class="top-bar-left show-for-medium-only show-for-small-only">
-        </div>
         <div class="top-bar-left">
-          <ul class="dropdown menu hide-for-medium-only hide-for-small-only" data-dropdown-menu>
+            <ul class="dropdown menu hide-for-medium" data-dropdown-menu>
+                <li><a class="top-bar-list" href="/">这可是只有手机才能看得到的</a></li>
+            </ul>
+          <ul class="dropdown menu hide-for-small-only" data-dropdown-menu>
             <li><a class="top-bar-list" href="/">首页</a></li>
             <li><a class="top-bar-list" href="/good">市场</a></li>
-            @if(Session::has('user_id'))
-              <li><a class="top-bar-list" href="/good/my">我的商品</a></li>
-            @endif
           </ul>
         </div>
-        <div class="top-bar-right">
-          <ul class="dropdown menu hide-for-small-only" data-dropdown-menu>
+        <div class="top-bar-right hide-for-small-only">
+          <ul class="dropdown menu hide-for-small-only top-bar-menu" data-dropdown-menu>
             @if(Session::has('user_id'))
-              @if(Session::has('is_admin'))
-                <li><a class="top-bar-list" href="/admin">管理员页面</a></li>
-              @endif
-              <li><a class="top-bar-list" id="top-bar-avatar"
-                     href="/user/{{Session::get('user_id')}}"><img
-                          src="/avatar/{{ Session::get('user_id') }}/54/54"></a></li>
+              <li><a class="top-bar-avatar" href="/user/{{Session::get('user_id')}}"><img src="/avatar/{{ Session::get('user_id') }}/54/54"/></a></li>
               <li><a class="top-bar-list" href="/message">消息</a></li>
-              <li><a class="top-bar-list" href="/user/fav">收藏夹</a></li>
-              <li><a href="/good/add" class="button">出售</a></li>
+              <li><a class="top-bar-list" href="/good/my">我的商品</a></li>
+              <li><a href="/good/add" class="top-bar-button">出售</a></li>
             @else
               <li><a class="top-bar-list" href="/register">注册</a></li>
               <li><a class="top-bar-list" href="/login">登录</a></li>
@@ -39,9 +32,11 @@
     </div>
   </div>
 </div>
-<div class="banner">
+<div class="banner-back">
+    <div class="banner">
+    </div>
 </div>
-<div class="row">
+<div class="row search-bar">
   <div class="medium-10 medium-centered columns">
     @include('layout.search')
   </div>
