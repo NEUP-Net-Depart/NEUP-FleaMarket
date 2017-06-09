d0de18d163a08ecbd31c63e507d3b65426270044
catsworld <519418441@qq.com>
2017-06-09 16:09:25 +0800

    完善了header与footer的内容 修改了搜索框的宽度 添加了composer.lock

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,4 +1,20 @@
-﻿<footer>
-	<div><a href="/">虽然你可能不信，但这是你梦寐以求的footer（的预览版）。</a>目前看来十分喜欢GitHub的footer的无底色设计，甚至可以不用考虑很多美观问题</div>
-	<div><span><a href="/">联系我们</a></span> <span><a href="/">客服申诉</a></span> <span><a href="/">GitHub</a></span>
+﻿<footer class="row hide-for-small-only">
+	<div class="medium-9 medium-centered columns">
+	<div class="float-left">
+		<span>© {{date("Y")}} 东北大学先锋网</span>
+		<span><a href="/">隐私政策</a></span>
+		<span><a href="/">服务条款</a></span>
+	</div>
+	<div class="float-right">
+		<span><a href="/">帮助文档</a></span>
+		<span><a href="/">申诉通道</a></span>
+		<span><a href="/">意见反馈</a></span>
+		<span><a href="/">关于我们</a></span>
+	</div>
+	</div>
+</footer>
+<footer class="row hide-for-medium">
+	<div class="small-11 small-centered columns">
+		<span><center>© {{date("Y")}} 东北大学先锋网</center></span>
+	</div>
 </footer>
--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -8,26 +8,48 @@
       <div class="medium-10 medium-offset-1 columns">
         <div class="top-bar-left">
             <ul class="dropdown menu hide-for-medium" data-dropdown-menu>
-                <li>
-					<a class="top-bar-list" data-toggle="offCanvas">≡</a>
-				</li>
-				<li><a class="top-bar-list" href="/">首页</a></li>
+              @if(Session::has('user_id'))
+                <li><a class="top-bar-list" data-toggle="offCanvas">≡</a></li>
+                <li><a class="top-bar-list" href="/">主页</a></li>
+                <li><a class="top-bar-list" href="/message">消息</a></li>
+              @else
+              <li class="is-dropdown-submenu-parent">
+                <a class="top-bar-list top-bar-dropdown" href="/login">登录&nbsp;</a>
+                <ul class="menu vertical top-bar-dropdown-menu">
+                  <li><a href="/login">普通登录</a></li>
+                  <li><a href="/register">校卡认证登录(SSO)</a></li>
+                </ul>
+              </li>
+              @endif
             </ul>
           <ul class="dropdown menu hide-for-small-only" data-dropdown-menu>
-            <li><a class="top-bar-list" href="/">首页</a></li>
-            {{--<li><a class="top-bar-list" href="/good">市场</a></li>--}}
+            <li><a class="top-bar-list" href="/">跳蚤市场</a></li>
           </ul>
         </div>
         <div class="top-bar-right hide-for-small-only">
-          <ul class="dropdown menu hide-for-small-only top-bar-menu" data-dropdown-menu>
+          <ul class="dropdown menu top-bar-menu" data-dropdown-menu>
             @if(Session::has('user_id'))
-              <li><a class="top-bar-avatar" href="/user/{{Session::get('user_id')}}"><img src="/avatar/{{ Session::get('user_id') }}/54/54"/></a></li>
+              <li>
+                <a class="top-bar-avatar" href="/user/{{Session::get('user_id')}}">
+                  <img src="/avatar/{{ Session::get('user_id') }}/54/54"/>
+                </a>
+                <ul class="menu vertical top-bar-dropdown-menu">
+                  <li><a href="/user">个人中心</a></li>
+                  <li><a href="/user/fav">收藏夹</a></li>
+                  <hr>
+                  <li><a href="/logout">登出</a></li>
+                </ul>
+              </li>
               <li><a class="top-bar-list" href="/message">消息</a></li>
-              <li><a class="top-bar-list" href="/user/fav">收藏夹</a></li>
               <li><a href="/good/add" class="top-bar-button">出售</a></li>
             @else
-              <li><a class="top-bar-list" href="/register">注册</a></li>
-              <li><a class="top-bar-list" href="/login">登录</a></li>
+              <li class="is-dropdown-submenu-parent">
+                <a class="top-bar-list top-bar-dropdown" href="/login">登录&nbsp;</a>
+                <ul class="menu vertical top-bar-dropdown-menu">
+                  <li><a href="/login">普通登录</a></li>
+                  <li><a href="/register">校卡认证登录(SSO)</a></li>
+                </ul>
+              </li>
             @endif
           </ul>
         </div>
@@ -39,16 +61,38 @@
 	<div class="banner">
 	</div>
 </div>
-<div class="row search-bar">
+<div class="row search-bar hide-for-small-only">
   <div class="medium-10 medium-centered columns">
-    @include('layout.search')
+    <div class="medium-12 columns">
+      <form action="/good" method="GET">
+        <div class="input-group float-right" id="top-bar-quick-access">
+            <input class="input-group-field" type="search" name="query" id="searchq" placeholder="开始交易吧( '﹃'⑉)" value="<?php if(isset($_GET['query'])){echo $_GET['query']; } ?>"/>
+            <div class="input-group-button">
+                <input type="submit" class="button" value="G♂"/>
+            </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
+<div class="row search-bar hide-for-medium">
+  <div class="medium-10 medium-centered columns">
+      <form action="/good" method="GET">
+        <div class="input-group float-right" id="top-bar-quick-access-small">
+            <input class="input-group-field" type="search" name="query" id="searchq" placeholder="开始交易吧( '﹃'⑉)" value="<?php if(isset($_GET['query'])){echo $_GET['query']; } ?>"/>
+            <div class="input-group-button">
+                <input type="submit" class="button" value="G♂"/>
+            </div>
+        </div>
+      </form>
   </div>
 </div>
   <div class="off-canvas position-left" id="offCanvas" data-off-canvas>
     <ul class="vertical menu">
         @if(Session::has('user_id'))
-          <li><a href="/user/{{Session::get('user_id')}}"><img src="/avatar/{{ Session::get('user_id') }}/220/220"/></a></li>
-          <li><a href="/message">消息</a></li>
+          <li><a href="/user/{{Session::get('user_id')}}"><img src="/avatar/{{ Session::get('user_id') }}/220/220"/><br/><br/>个人中心</a></li>
+          <li><a href="/logout">登出</a></li>
+          <hr>
           <li><a href="/user/fav">收藏夹</a></li>
           <li><a href="/good/add" class="top-bar-button">出售</a></li>
         @else
