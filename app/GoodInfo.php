d682a95c64a0cc8ea65ea5e942fa7a81a639cc28
yuki <yukiyukiyu@github.com>
2017-06-13 20:30:19 +0800

    添加了对商品的封禁

--- app/GoodInfo.php
+++ app/GoodInfo.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class GoodInfo extends Model
 {
-    protected $table='good_info';
+	protected $table='good_info';
+	public function user()
+	{
+		return $this->belongsTo('App\User');
+	}
 }
