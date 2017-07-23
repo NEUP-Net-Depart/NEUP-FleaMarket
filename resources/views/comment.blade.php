b40fd051371dc53f970f27f01023a5c89e933bad
LI JIAHAO <lijiahao99131@gmail.com>
2017-07-23 14:47:37 +0800

    refine comment

--- resources/views/comment.blade.php
+++ resources/views/comment.blade.php
@@ -4,13 +4,21 @@
 
 @section('content')
 
-<div class="row">
-	<form action="/sendComment/{{ $trans_id }}" method="POST">
-		{!! csrf_field() !!}
-		<label class="right-inline">评价:</label>
-		<textarea name="comment"></textarea>
-		<input type="submit" class="button" value="确认">
-	</form>
-</div>
+
+    <form action="/sendComment/{{ $trans_id }}" method="POST">
+        {!! csrf_field() !!}
+        <div class="row form-group">
+            <div class="col col-xs-12 col-sm-8 col-md-6">
+                <label class="right-inline">评价:</label>
+                <textarea name="comment" class="form-control"></textarea>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col col-xs-12">
+                <input type="submit" class="btn btn-primary" value="确认">
+            </div>
+        </div>
+    </form>
+
 
 @endsection
