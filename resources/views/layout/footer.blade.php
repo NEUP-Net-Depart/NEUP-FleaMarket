e3030f25ea526be1574c1b7e9f1e0c89c2d93b20
LI JIAHAO <lijiahao99131@gmail.com>
2017-09-27 21:12:47 +0800

    modify footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,7 +1,7 @@
 ﻿﻿<p></p>
 
 <div class="footer container">
-    @if(env('APP_DEBUG')) <div class="row"><div class="mx-auto">当前处于调试模式</center></div></div> @endif
+    @if(env('APP_DEBUG')) <div class="row"><div class="mx-auto">当前处于调试模式</div></div> @endif
     @if(env('SHOW_VER')) <div class="row"><div class="mx-auto">程序版本：{{ config('app.version') }}&nbsp;&nbsp; 文件版本：<a href="https://github.com/NEUP-Net-Depart/NEUP-FleaMarket/commit/{{ explode(' ', exec('git log --pretty=oneline -1'))[0] }}">{{ exec('git log --abbrev-commit --pretty=oneline -1') }}</a></div></div> @endif
     <div class="d-none d-md-flex row">
         <ul class="col-auto">
@@ -16,15 +16,17 @@
             <li><a href="https://github.com/NEUP-Net-Depart/NEUP-FleaMarket">关于我们</a></li>
         </ul>
     </div>
-    <!--<div class="row"><div class="mx-auto">© {{date("Y")}} 先锋网</div></div>-->
+    <!--<div class="row"><div class="mx-auto">© </div></div>-->
     <div class="d-md-none row">
         <ul class="col-auto mr-auto">
-            <li><a href="/faq">帮助文档</a></li>
-            <li><a href="/help#complain">申诉通道</a></li>
+            <li>© {{date("Y")}} <a href="https://github.com/NEUP-Net-Depart/NEUP-FleaMarket">东北大学先锋网</a></li>
+
         </ul>
         <ul class="col-auto ml-auto">
+            <li><a href="/faq">帮助文档</a></li>
+            {{--<li><a href="/help#complain">申诉通道</a></li>--}}
             <li><a href="/help#bug">意见反馈</a></li>
-            <li><a href="https://github.com/NEUP-Net-Depart/NEUP-FleaMarket">关于我们</a></li>
+            {{--<li><a href="https://github.com/NEUP-Net-Depart/NEUP-FleaMarket">关于我们</a></li>--}}
         </ul>
     </div>
 </div>
